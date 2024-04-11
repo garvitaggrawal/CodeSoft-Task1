09218de5873c763be44754a9b401e5cdada3b177
garvitaggrawal <162052258+garvitaggrawal@users.noreply.github.com>
2024-04-11 22:33:40 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,16 @@
+a=int(input("Enter 1st number "))
+b=int(input("Enter 2nd number "))
+c=input("Choose operration(+,-,*,/)")
+
+if(c=='+'):
+        print("Result=",a+b)
+elif(c=='-'):
+        print("Result=",a-b)
+elif(c=='*'):
+        print("Result=",a*b)
+elif(c=='/'):
+        print("Result=",a/b)
+else:
+    
+        print("Wrong operation choose")
+    
